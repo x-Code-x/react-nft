d9350af26482517a1fcce0eaaaf56f7bfef4b9f4
Code X <724723+x-Code-x@users.noreply.github.com>
2022-03-23 00:02:00 +1100

    Update NoDerpiesNotification.js

--- frontend-react/src/components/NoDerpiesNotification.js
+++ frontend-react/src/components/NoDerpiesNotification.js
@@ -2,7 +2,7 @@ const NoDerpiesNotification = ({ setShowGallery }) => {
   return (
     <div className="notification is-danger is-light container mt-5">
       <button className="delete" onClick={() => setShowGallery(false)}></button>
-      <span>This account does not own any Whoopsie Derpies :(</span>
+      <span>This account does not own any Mystery Boxes :(</span>
     </div>
   );
 };
