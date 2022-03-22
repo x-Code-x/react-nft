0b47949e0add2df1d3edb2c6ef6056fbbbe3c283
Code X <724723+x-Code-x@users.noreply.github.com>
2022-03-22 23:24:33 +1100

    Update MetaMaskButton.js

--- frontend-react/src/components/MetaMaskButton.js
+++ frontend-react/src/components/MetaMaskButton.js
@@ -1,11 +1,11 @@
-import FoxMetamask from "../img/metamysteryfox.png";
+import MetaMysteryFox from "../img/metamysteryfox.png";
 
 const MetaMaskButton = ({ isConnecting, connectWalletHandler }) => {
   return (
     <>
       <button className={`button p-6 mt-6 ${isConnecting ? "is-loading" : ""}`} onClick={connectWalletHandler}>
         <figure className="image is-96x96 mr-2 custom-mobile-metamask-fox">
-          <img src={metamysteryfox} />
+          <img src={MetaMysteryFox} />
         </figure>
         <span className="is-uppercase is-size-7-mobile">Connect With Metamask</span>
       </button>
