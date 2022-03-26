d0714cbaf7b6de3ad9bfc8f49ac427e360b11b1a
Code X <724723+x-Code-x@users.noreply.github.com>
2022-03-26 14:50:11 +1100

    Update RinkebyWarning.js

--- frontend-react/src/components/RinkebyWarning.js
+++ frontend-react/src/components/RinkebyWarning.js
@@ -1,8 +1,8 @@
 const RinkebyWarning = () => {
   return (
     <div className="notification is-danger has-text-centered mb-0">
-      WARNING: You are connected to a network that <strong>is not</strong> Rinkeby. Change your network in MetaMask to
-      avoid doing something derpie!
+      WARNING: You are connected to a network that <strong>is not</strong> Binance Smart Chain Testnet. Change your network in MetaMask to
+      avoid your money disappearing in a puff of smoke!
     </div>
   );
 };
