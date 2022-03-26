ee156c8b010af204902640665bfdddd2881612a2
Code X <724723+x-Code-x@users.noreply.github.com>
2022-03-26 15:25:31 +1100

    Update UserMysteryBox.js

--- frontend-react/src/components/UserMysteryBox.js
+++ frontend-react/src/components/UserMysteryBox.js
@@ -1,34 +1,34 @@
 import Logo from "../img/Logo.png";
 import Attribute from "./Attribute";
 
-const UserMystryBoxes = ({ mysteryBoxDetails }) => {
-  const imgURI = `https://ipfs.io/ipfs/${mysteryBoxDetails.uriJSON.image.split("").splice(7).join("")}`;
-  const style = { backgroundColor: `#${mysteryBoxDetails.uriJSON.background_color}` };
+const UserDerpy = ({ derpieDetails }) => {
+  const imgURI = `https://ipfs.io/ipfs/${derpieDetails.uriJSON.image.split("").splice(7).join("")}`;
+  const style = { backgroundColor: `#${derpieDetails.uriJSON.background_color}` };
   return (
     <>
       <div className="card custom-card">
         <div className="card-image">
           <figure style={style} className="image is-4by4">
-            <img src={imgURI} alt="Mystery Box Image" />
+            <img src={imgURI} alt="Derpie Image" />
           </figure>
         </div>
         <div className="card-content">
           <div className="media">
             <div className="media-left">
               <figure className="image is-48x48">
-                <img src={Logo} alt="Myster Box Logo" />
+                <img src={Logo} alt="Whoosie Derpies Logo" />
               </figure>
             </div>
             <div className="media-left has-text-left">
-              <p className="title is-4 mb-0 is-capitalized">{mysteryBoxDetails.uriJSON.name}</p>
-              <p className="">{`Token ID: ${mysteryBoxDetails.tokenId}`}</p>
+              <p className="title is-4 mb-0 is-capitalized">{derpieDetails.uriJSON.name}</p>
+              <p className="">{`Token ID: ${derpieDetails.tokenId}`}</p>
             </div>
           </div>
 
           <div className="content field is-grouped is-flex">
-            <Attribute mysteryBoxDetails={mysteryBoxDetailsDetails} traitNum={0} />
-            <Attribute mysteryBoxDetails={mysteryBoxDetailsDetails} traitNum={1} />
-            <Attribute mysteryBoxDetails={mysteryBoxDetailsDetails} traitNum={2} />
+            <Attribute derpieDetails={derpieDetails} traitNum={0} />
+            <Attribute derpieDetails={derpieDetails} traitNum={1} />
+            <Attribute derpieDetails={derpieDetails} traitNum={2} />
           </div>
         </div>
       </div>
