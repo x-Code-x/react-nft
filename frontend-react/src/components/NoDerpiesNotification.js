39924a480e64ea6a6e98d784f7ce7984a6b40a58
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-02 20:43:03 +1100

    Update NoDerpiesNotification.js

--- frontend-react/src/components/NoDerpiesNotification.js
+++ frontend-react/src/components/NoDerpiesNotification.js
@@ -1,10 +1,11 @@
-const NoDerpiesNotification = ({ setShowGallery }) => {
+import { NETWORK, NFT_NAME } from "../constants";
+const NoNFTNotification = ({ setShowGallery }) => {
   return (
     <div className="notification is-danger is-light container mt-5">
       <button className="delete" onClick={() => setShowGallery(false)}></button>
-      <span>This account does not own any Whoopsie Derpies :(</span>
+      <span>This account does not own any {NFT_NAME}s :(</span>
     </div>
   );
 };
 
-export default NoDerpiesNotification;
+export default NoNFTNotification;
