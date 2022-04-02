6f0f40662df2fa2d6a8a992330059436bd4e8556
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-03 00:49:24 +1100

    Update Mint.js

--- frontend-react/src/components/Mint.js
+++ frontend-react/src/components/Mint.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
-import UserDerpy from "./UserDerpy";
+import UserNFT from "./UserNFT";
 import MetamaskWarning from "./MetamaskWarning";
 import MintProgressNotification from "./MintProgressNotification";
 import ErrorMessage from "./ErrorMessage";
