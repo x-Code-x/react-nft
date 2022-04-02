b2f04338443a4c96d260de0207190afec13814f3
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-02 18:20:26 +1100

    Update NoBoxesNotification.js

--- frontend-react/src/components/NoBoxesNotification.js
+++ frontend-react/src/components/NoBoxesNotification.js
@@ -1,10 +1,10 @@
-const NoBoxesNotification = ({ setShowGallery }) => {
+const NoNFTNotification = ({ setShowGallery }) => {
   return (
     <div className="notification is-danger is-light container mt-5">
       <button className="delete" onClick={() => setShowGallery(false)}></button>
-      <span>This account does not own any Mystery Boxes :(</span>
+      <span>This account does not own any %NFT_NAME% :(</span>
     </div>
   );
 };
 
-export default NoBoxesNotification;
+export default NoNFTNotification;
