eb4b18e493845aa3562a8f28edc5f35de0293a42
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-02 20:33:06 +1100

    Update constants.js

--- frontend-react/src/constants.js
+++ frontend-react/src/constants.js
@@ -1,11 +1,16 @@
-//export const DERPIES_ADDRESS = process.env.REACT_APP_DERPIES_ADDRESS_RINKEBY; // deployment
-// export const DERPIES_ADDRESS = process.env.REACT_APP_DERPIES_ADDRESS_LOCALHOST; // development
-
 export const MYSTERY_BOX_ADDRESS = process.env.REACT_APP_MYSTERY_BOX_ADDRESS_BSCTEST; // BSC Testnet deployment
 //export const MYSTERY_BOX_ADDRESS = process.env.REACT_APP_MYSTERY_BOX_ADDRESS_RINKEBY; // Rinkeby deployment
 // export const MYSTERY_BOX__ADDRESS = process.env.REACT_APP_MYSTERY_BOX_ADDRESS_LOCALHOST; // development
 
+export const NFT_ADDRESS = process.env.REACT_APP_NFT_ADDRESS_NETWORK;
+
+export const DERPIES_ADDRESS = process.env.REACT_APP_DERPIES_ADDRESS_RINKEBY; // deployment
+// export const DERPIES_ADDRESS = process.env.REACT_APP_DERPIES_ADDRESS_LOCALHOST; // development
 export const VRFCOORDINATORMOCK_ADDRESS_LOCALHOST = process.env.REACT_APP_VRFCOORDINATORMOCK_ADDRESS_LOCALHOST;
 
 const MINUTES_TO_MILLISECONDS = 60 * 1000;
 export const CHAINLINK_WAIT_TIME_MINUTES = 0.5 * MINUTES_TO_MILLISECONDS;
+
+export const PROJECT_NAME = "Project Name"
+export const NETWORK = "BSC Testnet"
+export const NFT_NAME = "NFT_NAME"
