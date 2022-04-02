626622e1cbd981bbd2ace77dfee9ef9a10d9a60d
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-02 16:40:41 +1100

    Update Attribute.js

--- frontend-react/src/components/Attribute.js
+++ frontend-react/src/components/Attribute.js
@@ -1,8 +1,8 @@
-const Attribute = ({ derpieDetails, traitNum }) => {
+const Attribute = ({ boxDetails, traitNum }) => {
   return (
     <div className="custom-attribute">
-      <span className="custom-attribute__trait">{derpieDetails.uriJSON.attributes[traitNum].trait_type}</span>
-      <span className="custom-attribute__value">{derpieDetails.uriJSON.attributes[traitNum].value}</span>
+      <span className="custom-attribute__trait">{boxDetails.uriJSON.attributes[traitNum].trait_type}</span>
+      <span className="custom-attribute__value">{boxDetails.uriJSON.attributes[traitNum].value}</span>
     </div>
   );
 };
