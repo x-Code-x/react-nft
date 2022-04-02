7667b6f0adabef1ee1d938aa244584df079d2759
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-02 22:58:45 +1100

    Update Navbar.js

--- frontend-react/src/components/Navbar.js
+++ frontend-react/src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = (props) => {
 
   return (
     <>
-      <nav className="navbar" role="navigation" aria-label="main navigation">
+      <nav className="navbar has-background-grey-darker" role="navigation" aria-label="main navigation">
         <div className="navbar-brand is-flex is-align-items-center">
           <figure className="image is-64x64 custom-nav-img pl-3 is-flex is-align-items-center">
             <img className="" src={Logo} />
