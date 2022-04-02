0cb32e1b26ed1323701c43ecc34c64181757a9f7
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-02 21:06:48 +1100

    Update MetamaskWarning.js

--- frontend-react/src/components/MetamaskWarning.js
+++ frontend-react/src/components/MetamaskWarning.js
@@ -1,3 +1,4 @@
+import { PROJECT_NAME } from "../constants";
 const MetamaskWarning = () => {
   return (
     <div className="notification is-danger container mt-5">
@@ -5,7 +6,7 @@ const MetamaskWarning = () => {
       <a href="https://metamask.io/" target="_blank">
         MetaMask extension
       </a>{" "}
-      installed to interact with Whoopsie Derpies.
+      installed to interact with {PROJECT_NAME}.
     </div>
   );
 };
