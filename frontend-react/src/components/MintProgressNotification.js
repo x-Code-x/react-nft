8d65a533908479a294306382613ca1e4713e0a4f
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-02 21:08:32 +1100

    Update MintProgressNotification.js

--- frontend-react/src/components/MintProgressNotification.js
+++ frontend-react/src/components/MintProgressNotification.js
@@ -1,3 +1,4 @@
+import { PROJECT_NAME, NETWORK, NFT_NAME } from "../constants";
 import Countdown from "react-countdown";
 
 const MintProgressNotification = ({ mintWaitTimer, setSelectedTab, transactionHash }) => {
@@ -10,8 +11,8 @@ const MintProgressNotification = ({ mintWaitTimer, setSelectedTab, transactionHa
   return (
     <div className="notification is-primary is-light container mt-5">
       <p className="mb-3">
-        Please wait while your transaction is processed by the ethereum rinkeby network. You can view your pending
-        transaction on etherscan here:{" "}
+        Please wait while your transaction is processed by the {NETWORK} network. You can view your pending
+        transaction on the appropriate blockchain explorer here:{" "}
         <a className="custom-word-wrap" href={`https://rinkeby.etherscan.io/tx/${transactionHash}`} target="_blank">
           https://rinkeby.etherscan.io/tx/{transactionHash}
         </a>
@@ -21,11 +22,11 @@ const MintProgressNotification = ({ mintWaitTimer, setSelectedTab, transactionHa
         <a href="https://docs.chain.link/docs/chainlink-vrf/" target="_blank">
           Chainlink VRF
         </a>{" "}
-        to respond to the Derpies contract with a random number. Your new Derpie will show here in:{" "}
+        to respond to the {PROJECT_NAME} contract with a random number. Your new {NFT_NAME} will show here in:{" "}
         <Countdown date={mintWaitTimer} />
       </p>
       <p>
-        Admire your other Derpies in the <a onClick={galleryClickHandler}>Gallery</a> while you wait!
+        Admire your other {NFT_NAME} in the <a onClick={galleryClickHandler}>Gallery</a> while you wait!
       </p>
     </div>
   );
